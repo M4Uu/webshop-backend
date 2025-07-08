c25a3c6808fc73f4ccab004a3215fa6e823cad39
cruzmlathuler@gmail.com <cruzmlathulerie@gmail.com>
2025-07-07 23:10:19 -0400

    test input...

--- src/models/postgre/users.ts
+++ src/models/postgre/users.ts
@@ -47,6 +47,7 @@ export class UserModel {
         FROM wp_usuarios
         WHERE correo = TRIM($1);
       `;
+      return input;
 
       const result = await client.query<any>(query, [input.correo])
       return result;
