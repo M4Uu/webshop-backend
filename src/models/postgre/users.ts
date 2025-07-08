7ccfe825b6230cc44667f7d704ed6fa58a7d74c5
cruzmlathuler@gmail.com <cruzmlathulerie@gmail.com>
2025-07-07 22:40:35 -0400

    test interface on client.query

--- src/models/postgre/users.ts
+++ src/models/postgre/users.ts
@@ -1,5 +1,6 @@
 import { Pool } from 'pg';
 import bcrypt from 'bcryptjs';
+import { UserData } from 'interface/users';
 
 
 export const dbConfig = {
@@ -49,7 +50,7 @@ export class UserModel {
         WHERE correo = $1;
       `;
 
-      const result = await client.query<any>(query, [input.correo])
+      const result = await client.query<UserData>(query, [input.correo])
       return result;
       const user = result.rows[0];
       const validatePassword = await comparePassword(input.credencial, user.credencial);
