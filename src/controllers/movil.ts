16ba885cdd9e4f3e04a575dc017d9fb988b4c441
cruzmlathuler@gmail.com <cruzmlathulerie@gmail.com>
2025-07-17 08:58:25 -0400

    feat: bit canges

--- src/controllers/movil.ts
+++ src/controllers/movil.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express"
 import fs from 'fs';
 
 export class MovilController {
-  protected json_path_banklist = 'C:/Users/USUARIO/Desktop/webshop-workspace/webshop-backend/src/models/json/bancos.json'
+  protected json_path_banklist = '../models/json/bancos.json'
 
   listbanks = async (_req: Request, res: Response) => {
     try {
